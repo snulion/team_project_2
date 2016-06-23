b102256059bed8bcc43c0558b31562cfdf4e6a6c
jin0.u <me@jin0u.com>
2016-06-23 21:54:05 +0900

    Add db 'image_mainposter' in Movie Model

--- db/migrate/20160623092550_create_movies.rb
+++ db/migrate/20160623092550_create_movies.rb
@@ -1,12 +1,13 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-      
+
       t.string :name
       t.text :intro
       t.integer :rate_star
       t.integer :rate_ppl
       t.text :etc
+      t.string :image_mainposter
 
       t.timestamps null: false
     end
